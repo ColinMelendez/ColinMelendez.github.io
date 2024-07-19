1015b4960ee6ade6f5a96248fbfc7adf903aecb2
Colin <cwrmcolin@gmail.com>
2024-07-19 11:03:04 -0700

    added a fun console message and continued with the main content

---
+++ src/console-message.js
@@ -0,0 +1,32 @@
+// Just having a bit of fun.
+
+let letterStyles = [
+  "color:rgb(93, 0, 255); font-weight: bold;",
+  "color:rgb(212, 0, 255); font-weight: bold;",
+  "color:rgb(255, 0, 132); font-weight: bold;",
+  "color:rgb(255, 128, 0); font-weight: bold;",
+  "color:rgb(255, 213, 0); font-weight: bold;",
+];
+
+console.log("\
+\n\
+%c   .o88b. %c  .d88b. %c db     %c d888888b%c d8b   db\n\
+%c  d8P  Y8 %c .8P  Y8.%c 88     %c   `88'  %c 888o  88\n\
+%c  8P      %c 88    88%c 88     %c    88   %c 88V8o 88\n\
+%c  8b      %c 88    88%c 88     %c    88   %c 88 V8o88\n\
+%c  Y8b  d8 %c `8b  d8'%c 88booo.%c   .88.  %c 88  V888\n\
+%c   `Y88P' %c  `Y88P' %c Y88888P%c Y888888P%c VP   V8P\n",
+...letterStyles, ...letterStyles, ...letterStyles, ...letterStyles, ...letterStyles, ...letterStyles);
+
+console.log("%c\
+  \n\
+  Glad you're poking around!\n\
+  I also like to check out how things work under the hood.\n\
+  \n\
+  Make sure to reach out if you wanna build cool stuff together!\n\
+  \n\
+  - - - https://github.com/ColinMelendez\n\
+  \n\
+  - - - https://www.linkedin.com/in/colin-melendez-8b7378222/\n\
+  \n",
+  "color:rgb(0, 157, 255);")
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './style.css'
 // Import the theme switcher script so it gets bundled with the application
 import './minimal-theme-switcher.js'
 
+import './console-message.js'
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1><span class="handwave">👋</span>Vite + TypeScript</h1>
